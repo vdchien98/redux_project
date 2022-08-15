9ea9613f33ab2d18a1dc037d9faa488210a3b736
dangchien <82793130+vdchien98@users.noreply.github.com>
2022-08-15 15:34:22 +0700

    useDispatch

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import { useDispatch } from 'react-redux';
+import { login, logout } from '../features/user';
 function Login() {
+    const dispatch = useDispatch();
     return (
         <div>
-            <button>Login</button>
+            <button
+                onClick={() => {
+                    dispatch(login({ name: 'Vdchien98', age: 24, email: 'vudangchien98@gmail.com' }));
+                }}
+            >
+                Login
+            </button>
+            <button
+                onClick={() => {
+                    dispatch(logout());
+                }}
+            >
+                Logout
+            </button>
         </div>
     );
 }
--- src/features/user.js
+++ src/features/user.js
@@ -1,15 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit';
-
+const initialStateValue = { name: '', age: 0, email: '' };
 export const userSlice = createSlice({
     name: 'user',
-    initialState: { value: { name: 'Vdchien98', age: 0, email: 'vudangchien98@gmail.com' } },
+    initialState: { value: initialStateValue },
     reducers: {
         login: (state, action) => {
             state.value = action.payload;
         },
+        logout: (state, action) => {
+            state.value = initialStateValue;
+        },
     },
 });
 
-export const { login } = userSlice.actions;
+export const { login, logout } = userSlice.actions;
 
 export default userSlice.reducer;
