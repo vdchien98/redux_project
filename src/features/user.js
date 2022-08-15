e163eb7e4d1b3f56ad86c7685ac0630be19512cb
dangchien <82793130+vdchien98@users.noreply.github.com>
2022-08-15 15:24:13 +0700

    useSelector

--- src/features/user.js
+++ src/features/user.js
@@ -2,11 +2,14 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export const userSlice = createSlice({
     name: 'user',
-    initialState: { value: { name: '', age: 0, email: '' } },
+    initialState: { value: { name: 'Vdchien98', age: 0, email: 'vudangchien98@gmail.com' } },
     reducers: {
         login: (state, action) => {
             state.value = action.payload;
         },
     },
 });
+
+export const { login } = userSlice.actions;
+
 export default userSlice.reducer;
